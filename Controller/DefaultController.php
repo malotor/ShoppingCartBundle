1d992ede3b4aa4c9ac444d5c5d7718cf4fda5b1b
vagrant <vagrant@cibbva>
2015-01-16 14:11:45 +0000

    Inject request

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,16 +4,25 @@ namespace Cupon\ShoppingCartBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class DefaultController extends Controller
 {
+  public function __construct(RequestStack $requestStack)
+  {
+    $this->requestStack = $requestStack;
+    $this->request = $this->requestStack->getCurrentRequest();
+  }
+
   public function addToCartAction($id)
   {
     $ecommerce = $this->container->get('cupon_shopping_cart.ecommerce');
     try {
       $ecommerce->addProductToCart($id);
       $cartItems = $ecommerce->getCartItems();
-      var_dump($cartItems);
+
+      $this->getRequest()->getSession()->setFlash('notice', "your_message");
+
     } catch (Exception $e) {
 
     }
